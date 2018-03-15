b90cb1c5e3d9a211622c32fae3e60b1887b49c79
Raul Sanchez Martin <r.sanchezma.2016@alumnos.urjc.es>
2018-03-15 20:49:06 +0100

    section 4 exercises

---
+++ Deep_Learning_A_Z/Volume 2 - Unsupervised Deep Learning/Part 4 - Self Organizing Maps (SOM)/Section 16 - Building a SOM/som_project/som.py
@@ -0,0 +1,10 @@
+# Self Organizing Maps
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv("Credit_Card_Applications.csv")
+
